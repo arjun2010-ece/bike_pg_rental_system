443f0f4dbb7d0a16af9b3f9ae64ecc02a620642c
Arjun sah <arjun2010.ece@gmail.com>
2024-01-31 18:15:03 +0000

    updated admin signup page

--- src/pages/PG-Adminpage.jsx
+++ src/pages/PG-Adminpage.jsx
@@ -2,34 +2,34 @@ import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import "./PGAdminpage.css";
 
-
 const PGAdminpage = () => {
   return (
-    <Fragment>
-      <div className="pg-admin-form">
-        <div className="pg-admin">
-          <div className="form">
-
-            <label>Email : </label>
-            <input type="text" placeholder="Enter your email" />
-            <br/><br/>
-
-            <label>Mobile No . </label>
-            <input type="text" placeholder="Enter your email" />
-            <br/><br/>
-
-            <label>Password : </label>
-            <input type="text" placeholder="Enter your password" />
-            <br/><br/>
-
-            <Link to="/pg-rental/admin/login">Log in</Link>
-            <Link to="/pg-rental/admin/signup">Register now</Link>
-
-          </div>
+    <div className="pg-admin-form">
+      <form className="pg-admin form">
+        <div className="form-group">
+          <label>Email : </label>
+          <input type="text" placeholder="Enter your email" />
         </div>
-      </div>
-    </Fragment>
-  )
-}
 
-export default PGAdminpage
+        <div className="form-group">
+          <label>Mobile No . </label>
+          <input type="text" placeholder="Enter your email" />
+        </div>
+
+        <div className="form-group">
+          <label>Password : </label>
+          <input type="text" placeholder="Enter your password" />
+        </div>
+
+        {/* <Link to="/pg-rental/admin/login">Log in</Link>
+        <Link to="/pg-rental/admin/signup">Register now</Link> */}
+        <div className="btn-group">
+          <button>Log in</button>
+          <button>Register</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PGAdminpage;
